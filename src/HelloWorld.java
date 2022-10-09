169477b32670e30050cb953a63da4ec8d8aa8ef1
francesperez <frances.perez141@gmail.com>
2022-10-09 12:12:00 -0500

    Exercise on Java syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,51 @@
 public class HelloWorld {
     public static void main(String[] args) {
 //        The main method is where your code begins to run.
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
 //        Click on green play button next to the main line. This will print Hello World in the terminal.
 
 //        You will notice an 'out' folder will get created when you run this code. It is healthy practice to delete
 //        this out folder, as it will reappear each time you run the code again. Nothing will happen if you delete it.
+
+        int myFavoriteNumber = 144;
+        System.out.println(myFavoriteNumber);
+
+        //String myString = "This is #2 in the exercise";
+        //System.out.println(myString);
+        //newString = (char)myString;
+        //String myString = 3.14159;
+        //System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        This prints the number first, then adds one to it
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+////        This adds a number first, then prints it
+//        System.out.println(x);
+
+        //class imaClass;
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//Rewrite the following expressions using the relevant shorthand assignment operators:
+
+
+        int x = 10;
+        int y = 2;
+       y -= (x /= y);
+        System.out.println(y);
+
+
+
+
+
     }
 }
