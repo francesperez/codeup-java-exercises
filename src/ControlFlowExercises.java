e1598939140898c21094ada05a259853f8648f8f
francesperez <frances.perez141@gmail.com>
2022-10-12 16:56:40 -0500

    Refactored code to use for loops instead

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,31 +1,17 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        1. Loop Basics
-//        a. Do While
-// Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
-//        int i = 0;
-//        do {
-//            System.out.println(i * 2);
-//            i++;
-//        }
-//        while (i <= 50);
-//    }
+//        c. For
+//        Refactor the previous two exercises to use a for loop instead.
+        int counter = 100;
 
-        //Alter your loop to count backwards by 5's from 100 to -10
 
-//        int i = 20;
-//        do {
-//            System.out.println(i * 5);
-//            i--;
-//        }
-//        while (i >= -2);
-//    }
-
-// Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
-        long i = 2L;
-        do {
+        for(int i = 100; i >= -10; i -= 5) {
             System.out.println(i);
-            i *= i;
-        } while (i < 1000000L);
+        }
+
+        for(long i = 2L; i < 1000000; i *= i) {
+            System.out.println(i);
+        }
     }
 }
