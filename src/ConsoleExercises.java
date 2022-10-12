64e72fdf21a736c8f11d36e632c4ffb27a393e06
francesperez <frances.perez141@gmail.com>
2022-10-12 14:49:27 -0500

    Exercise on console io

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 //        double pi = 3.14159;
-//        System.out.printf("The value of pi is approximately %3.2f.%n", pi);
+//        System.out.printf("The value of pi is approximately %.2f.%n", pi);
         Scanner scanner = new Scanner(System.in);
 //        System.out.println("Enter a number:");
 //        int userNumber = scanner.nextInt();
@@ -13,13 +13,12 @@ public class ConsoleExercises {
 
 
 //        scanner.nextLine();
-//        System.out.println("Enter a word:");
-//        String userInput1 = scanner.nextLine();
-//        System.out.println("Enter another word:");
-//        String userInput2 = scanner.nextLine();
-//        System.out.println("Enter one last word:");
-//        String userInput3 = scanner.nextLine();
-//        System.out.printf("Your words together make the following sentence: %s %s %s.", userInput1, userInput2, userInput3 );
+        System.out.println("Enter three words:");
+        String userInput1 = scanner.next();
+        String userInput2 = scanner.next();
+        String userInput3 = scanner.next();
+        System.out.printf("Your words together make the following sentence: %n%s%n%s%n%s%n", userInput1, userInput2,
+        userInput3 );
 //
 //        scanner.nextLine();
 //        System.out.println("Enter a sentence:");
