d67f75be7c4a87c5c64efb63488b1c7628698341
francesperez <frances.perez141@gmail.com>
2022-10-14 09:55:01 -0500

    Finished StringExercise

---
+++ src/StringExercise.java
@@ -0,0 +1,7 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String education = "We don't need no education", control = "We don't need no thought control", look = "Check \"this\" out!, \"s inside of \"s!", windows = "In windows, the main drive is usually C:\\", backslashes = "I can do backslashes \\, double backslashes \\\\,\n and the amazing triple backslash \\\\\\!";
+        System.out.printf("%s%n%s%n%s%n%s%n%s%n", education, control, look, windows, backslashes);
+    } //End of main method
+} //End of class
