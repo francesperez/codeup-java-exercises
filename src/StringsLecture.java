9706f4c9c4758df6c447eb593b19552d1c08ba06
francesperez <frances.perez141@gmail.com>
2022-10-14 09:39:34 -0500

    Strings Lecutre notes

--- src/StringsLecture.java
+++ src/StringsLecture.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class StringsLecture {
     public static void main(String[] args) {
-        String teacher ="teacher";
-        System.out.println(teacher);
-        teacher = "not teacher";
+
+//========================STRING IMMUTABILITY======================================
+//    JAVA's way of saving memory. If there are multiple strings with the exact same character pattern, they will be
+//    sdtored in the same place.
+
+
         String javier = "Javier";
+        System.out.println(Integer.toHexString((System.identityHashCode(javier)))); //This is the best approximation
+        // we can get to converting the internal address of an object into a number. Again, internal address means
+        // when you save a variable, the computer has to know where in memory to look for that value.
         String anotherJavier = "Javier";
-        System.out.println(teacher);
+        System.out.println(Integer.toHexString((System.identityHashCode(anotherJavier))));
+//        In theory, the identityHashcode is more accurately identifying the address vs. hashCode.
+
         System.out.println(javier);
         System.out.println(javier.hashCode());
         System.out.println(anotherJavier.hashCode());
@@ -15,9 +23,30 @@ public class StringsLecture {
 //        saved in the String pool until the end of the program runs. As we understand it, if you declare another
 //        variable with the same string value, as we did above with javier and anotherJavier, both of them would be
 //        referring to the same place in memory.
+        javier = javier + " Ruedas";
+        anotherJavier = javier;
+
+        System.out.println(anotherJavier);
+        System.out.println(javier);
+        System.out.println(Integer.toHexString((System.identityHashCode(javier))));
+        System.out.println(Integer.toHexString((System.identityHashCode(anotherJavier))));
+
+        String doiwork = "Javier";
+        System.out.println(Integer.toHexString((System.identityHashCode(doiwork))));
+
+
+
         System.out.println(javier == anotherJavier);
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.next();
-        System.out.println(javier.equals(userInput));
+//        Why should we not use double equals? Because it does not reference the strings, but rather their references
+//        . Since they are both references to the same place in memory, it will come out true even though we changed
+//        javier to Javier Ruedas earlier.  We don't want to know if the reference addresses are the same, but that
+//        the strings are the same.
+
+//        Scanner scanner = new Scanner(System.in);
+//        String userInput = scanner.next();
+//        System.out.println(javier.equals(userInput));
+
+
+//    }
     }
 }
