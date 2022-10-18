2e1bff4d6d804b8a92221764ccd722ed2e5ddb11
francesperez <frances.perez141@gmail.com>
2022-10-18 11:22:57 -0500

    Completed part 1 of exercise: Object Basics

---
+++ src/Person.java
@@ -0,0 +1,35 @@
+public class Person {
+
+
+
+    private String name;
+
+
+//setter
+    public void setName(String name){
+//TODO: change the name field to the passed value
+        this.name = name;
+    }
+
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.printf("Someone told me your name is %s. Hello from me, your loyal Macbook!", name);
+    }
+
+
+//getter
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
+
+    public static void main(String[] args) {
+        Person name = new Person();
+        name.setName("Frances");
+        name.sayHello();
+    }
+
+
+
+
+}//class ending bracket
